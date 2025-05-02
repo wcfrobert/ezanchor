9745d224e81278fa9cee8c25fbcc5ffa5ed93fd5
Robert W <55780110+wcfrobert@users.noreply.github.com>
2025-05-01 23:35:40 -0700

    updated requirements.txt

--- ezanchor/__init__.py
+++ ezanchor/__init__.py
@@ -1,2 +1,10 @@
+"""
+ezanchor is a python package that can perform equipment anchorage demand calculations in all 360 degrees of load orientation.
+"""
+
+__version__ = "1.1.0"
+__author__ = "Robert Wang"
+__license__ = "MIT"
+
 import ezanchor.equipment
 import ezanchor.plotter
